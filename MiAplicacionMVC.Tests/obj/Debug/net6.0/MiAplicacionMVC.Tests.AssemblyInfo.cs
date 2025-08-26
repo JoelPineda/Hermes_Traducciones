93cbd24490919237a9cc5a56cab9f2443111259e
Joel Pineda <joel.pineda@corripio.com.do>
2025-08-26 19:30:47 -0400

    Add JUnitTestLogger package for CI test reports

--- MiAplicacionMVC.Tests/obj/Debug/net6.0/MiAplicacionMVC.Tests.AssemblyInfo.cs
+++ MiAplicacionMVC.Tests/obj/Debug/net6.0/MiAplicacionMVC.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiAplicacionMVC.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec1d78cdeb022a5c55dceb7fbd7f451bf58badeb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54d17d9861f5bb46f6c4d5858df0bb59b7b7d321")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiAplicacionMVC.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiAplicacionMVC.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- hermes_translations/obj/Debug/net5.0/hermes_translations.AssemblyInfo.cs
+++ hermes_translations/obj/Debug/net5.0/hermes_translations.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hermes_translations")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec1d78cdeb022a5c55dceb7fbd7f451bf58badeb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54d17d9861f5bb46f6c4d5858df0bb59b7b7d321")]
 [assembly: System.Reflection.AssemblyProductAttribute("hermes_translations")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hermes_translations")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
